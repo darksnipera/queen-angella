9a6455f5bc98ef10e6d42af752372533b0e5620b
darksnipera <99520125+darksnipera@users.noreply.github.com>
2022-02-13 09:51:14 +0400

    Update config.js

--- config.js
+++ config.js
@@ -48,7 +48,7 @@ module.exports = {
     BANMSG: process.env.BAN_MESSAGE === undefined ? 'default' : process.env.BAN_MESSAGE,
     BOTSK: process.env.BOT_NAME === undefined ? 'QUEEN ANGELLA' : process.env.BOT_NAME,
     LOGOSK: process.env.ALL_IMG === undefined ? 'https://i.pinimg.com/originals/0e/c8/8c/0ec88ca1469125fc11b4ce76830602f4.jpg' : process.env.ALL_IMG,
-    SLINK: process.env.SOCIAL_MEDIA === undefined ? 'https://wa.me/27634090203' : process.env.SOCIAL_MEDIA,
+    SLINK: process.env.SOCIAL_MEDIA === undefined ? 'https://wa.me/966568319347' : process.env.SOCIAL_MEDIA,
     TAGPLK: process.env.TAG_HEADER === undefined ? '*Attention please 📢*' : process.env.TAG_HEADER,
     AFKMSG: process.env.AFK_MESSAGE === undefined ? 'default' : process.env.AFK_MESSAGE,
     HANDLERS: process.env.HANDLERS === undefined ? '^[.!;]' : process.env.HANDLERS,
